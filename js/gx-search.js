a4364842112d8c11b863c046d4f4509e265b71dd
7ystar <997002885@qq.com>
2017-04-13 23:01:08 +0800

    下拉列表回不去

--- js/gx-search.js
+++ js/gx-search.js
@@ -1,6 +1,9 @@
 $(function(){
 	let dian=$(".search-sousuo input")
 	dian.click(function(){
-		$(this).parent().next(".xiala").css("display","none").eq($(this).index()).css("display","block");
+		$(this).parent().next(".xiala").eq($(this).index()).css("display","none");
+	})
+	dian.click(function(){
+		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
 	})
 })
