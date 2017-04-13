1e35afcb3016d62d09519f2b82c71d4d8496db62
Cocofaerie <517545519@qq.com>
2017-04-13 17:45:53 +0800

    'mingxingyuanchuang'

--- js/yy-sp.js
+++ js/yy-sp.js
@@ -1,21 +1,11 @@
 /**
  * Created by 完美睡姿 on 2017/4/12.
  */
-var myScroll;
-function loaded () {
-    myScroll = new IScroll('#wrapper', {
-        scrollX:true, //水平方向滚动，默认false
-        scrollY:false,
-        bounce:false,
-        // scrollbars: true,
-        mouseWheel: true,
-        interactiveScrollbars: true,
-        shrinkScrollbars: 'scale',
-        fadeScrollbars: true
-    });
-}
-
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, isPassive() ? {
-    capture: false,
-    passive: false
-} : false);
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    // paginationClickable: true,
+    spaceBetween: -1,
+    centeredSlides: true,
+    autoplayDisableOnInteraction: false,
+    loop:false,
+});
