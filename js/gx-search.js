4cf93d6b16e2650a4967abd2ddb4617678973a26
7ystar <997002885@qq.com>
2017-04-14 08:38:03 +0800

    购物车未完成

--- js/gx-search.js
+++ js/gx-search.js
@@ -1,9 +1,9 @@
 $(function(){
 	let dian=$(".search-sousuo input")
 	dian.click(function(){
-		$(this).parent().next(".xiala").eq($(this).index()).css("display","none");
+		$(this).parent().next(".xiala").focus.eq($(this).index()).css("display","none");
 	})
-	dian.click(function(){
-		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
-	})
+//	dian.click(function(){
+////		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
+//	})
 })
