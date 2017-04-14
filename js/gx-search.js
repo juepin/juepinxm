0c1583c6d96a184995cb213e7e7493f2b48a2a6a
7ystar <997002885@qq.com>
2017-04-14 08:47:02 +0800

    11

--- js/gx-search.js
+++ js/gx-search.js
@@ -1,13 +1,9 @@
 $(function(){
 	let dian=$(".search-sousuo input")
-	dian.click(function(){
-		$(this).parent().next(".xiala").focus.eq($(this).index()).css("display","none");
+	dian.focus(function(){
+		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
 	})
-<<<<<<< HEAD
-//	dian.click(function(){
-////		$(this).parent().next(".xiala").eq($(this).index()).css("display","block");
-//	})
-=======
-
->>>>>>> 8608318757a3d2aeefe2b4523e4647d143525529
+	dian.blur(function(){
+		$(this).parent().next(".xiala").eq($(this).index()).css("display","none");
+	})
 })
